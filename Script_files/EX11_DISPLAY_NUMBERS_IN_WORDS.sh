832880e842abc2556f9a27bc90c259c355f928d8
ASHIK JENLY <116492348+AshikJenly@users.noreply.github.com>
2023-02-25 13:00:50 +0530

    Update EX11_DISPLAY_NUMBERS_IN_WORDS.sh

--- Script_files/EX11_DISPLAY_NUMBERS_IN_WORDS.sh
+++ Script_files/EX11_DISPLAY_NUMBERS_IN_WORDS.sh
@@ -2,49 +2,49 @@
 
 printDigit(){
 
-d=$1
-w=$2
-case $d in 
-1) echo "one $w";;
-2) echo "two $w";;
-3) echo "three $w";;
-4) echo "four $w";;
-5) echo "five $w";;
-6) echo "six $w";;
-7) echo "seven $w";;
-8) echo "eight $w";;
-9) echo "nine $w";;
-esac
+    d=$1
+    w=$2
+    case $d in 
+        1) echo "one $w";;
+        2) echo "two $w";;
+        3) echo "three $w";;
+        4) echo "four $w";;
+        5) echo "five $w";;
+        6) echo "six $w";;
+        7) echo "seven $w";;
+        8) echo "eight $w";;
+        9) echo "nine $w";;
+    esac
 }
 PrintGreaterThan20(){
-d=$1 
+    d=$1 
 
-case $d in
-2) echo "twenty ";;
-3) echo "thirty ";;
-4) echo "forty ";;
-5) echo "fifty ";;
-6) echo "sixty ";;
-7) echo "seventy ";;
-8) echo "eighty ";;
-9) echo "ninety ";;
-esac
+    case $d in
+        2) echo "twenty ";;
+        3) echo "thirty ";;
+        4) echo "forty ";;
+        5) echo "fifty ";;
+        6) echo "sixty ";;
+        7) echo "seventy ";;
+        8) echo "eighty ";;
+        9) echo "ninety ";;
+    esac
 }
 PrintTenToNineTeen(){
-d=$1
-case $d in 
-10) echo "ten";;
-11) echo "eleven";;
-12) echo "twelve";;
-13) echo "thirteen";;
-14) echo "fourteen";;
-15) echo "fifteen";;
-16) echo "sixteen";;
-17) echo "seventeen";;
-18) echo "eighteen";;
-19) echo "nineteen";;
+    d=$1
+    case $d in 
+        10) echo "ten";;
+        11) echo "eleven";;
+        12) echo "twelve";;
+        13) echo "thirteen";;
+        14) echo "fourteen";;
+        15) echo "fifteen";;
+        16) echo "sixteen";;
+        17) echo "seventeen";;
+        18) echo "eighteen";;
+        19) echo "nineteen";;
 
-esac
+    esac
 }
 
 read -p "Enter any number between 1-9999 : " n
